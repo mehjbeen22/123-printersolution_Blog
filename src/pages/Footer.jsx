3caac8ae946f90f54bc46ae64cffb47896842024
mehjbeen22 <mehjbeen22@navgurukul.org>
2024-07-15 11:43:56 +0530

    initial commit

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#0c4a6e] text-gray-100 p-4 text-center">
+      <p className="mt-4">
+        Copyright © 2024 123-printersolution. All Rights Reserved.
+      </p>
+    </footer>
+  );
+}
